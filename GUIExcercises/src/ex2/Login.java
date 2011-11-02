aea5a1be0c2e4633d59aede75cbadb614c0f396a
Steven Ruppert <blendmaster@gmail.com>
2011-11-02 15:19:40 -0600

    removed unused imports

--- GUIExcercises/src/ex2/Login.java
+++ GUIExcercises/src/ex2/Login.java
@@ -1,15 +1,13 @@
 package ex2;
 
-import java.awt.BorderLayout;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.WindowConstants;
+
 
 public class Login extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 2835808078928828237L;
